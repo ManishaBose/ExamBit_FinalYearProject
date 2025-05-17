a1cdf47397a121d9d86c787036fba33864098461
MANISHA BOSE <manishabose.2001@gmail.com>
2025-05-17 07:57:10 +0530

    main commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONOGOURI: "mongodb+srv://admin:wUaQxIdNtC4exRsp@cluster0.gamcz.mongodb.net/exambit",
+    jwtPrivateKey: "shhh",
+    SENDGRID_API_KEY:
+      "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",
+    EMAIL: "manishabose.2001@gmail.com",
+  };
